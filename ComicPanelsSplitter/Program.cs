479eb37525b2bba4be73c3d64bb7875bb0496562
thieupepijn <thieupepijn@hotmail.com>
2020-02-27 22:35:04 +0100

    thirteenth commit

--- ComicPanelsSplitter/Program.cs
+++ ComicPanelsSplitter/Program.cs
@@ -4,6 +4,7 @@ using System.Drawing.Imaging;
 using System.IO;
 using System.Collections.Generic;
 using PanelSplitter;
+using System.Text;
 
 namespace ComicPanelsSplitter
 {
@@ -11,22 +12,25 @@ namespace ComicPanelsSplitter
     {
         static void Main(string[] args)
         {
+
+            if (args.Length < 2)
+            {
+                ShowUsage();
+                return;
+            }
+
             string imageFilePath = args[0];
             string exportPath = args[1];
 
             if (!File.Exists(imageFilePath))
             {
-                Console.WriteLine();
-                string errorMessage = string.Format("File {0} does not exist", imageFilePath);
-                Console.WriteLine(errorMessage);
+                WriteMessageToConsole(string.Format("File {0} does not exist", imageFilePath));
                 return;
             }
 
             if (!Directory.Exists(exportPath))
             {
-                Console.WriteLine();
-                string errorMessage = string.Format("Directory {0} does not exist", exportPath);
-                Console.WriteLine(errorMessage);
+                WriteMessageToConsole(string.Format("Directory {0} does not exist", exportPath));
                 return;
             }
 
@@ -35,17 +39,12 @@ namespace ComicPanelsSplitter
             {
                 SplitInPanels(imageFilePath, exportPath, regions);
             }
-            catch
+            catch(Exception ex)
             {
-                Console.WriteLine();
-                string errorMessage = string.Format("Something went wrong, cannot process image {0}", imageFilePath);
-                Console.WriteLine(errorMessage);
+                WriteMessageToConsole(string.Format("Something went wrong, cannot process image {0}", imageFilePath));
                 return;
             }
-
-            Console.WriteLine();
-            string message = string.Format("Image {0} splitted in {1} panels which were written to {2}", imageFilePath, regions.Count, exportPath);
-            Console.WriteLine(message);
+            WriteMessageToConsole(string.Format("Image {0} splitted in {1} panels which were written to {2}", imageFilePath, regions.Count, exportPath)); 
         }
 
         private static void SplitInPanels(string imageFilePath, string exportPath, List<FloodFilledRegion> regions)
@@ -67,6 +66,21 @@ namespace ComicPanelsSplitter
                 regions = FloodFilledRegion.SortRegions(regions);
                 Util.CutandWriteToFile(regions, comicPage, exportPath);
             }
+        }
+
+        private static void ShowUsage()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("ComicPanelSplitter, small tool that splits a comic-page into it's panels");
+            builder.AppendLine();
+            builder.AppendLine("Usage: ComicPanelSplitter.exe <ComicImageFile> <ExportPath>");
+            WriteMessageToConsole(builder.ToString());
+        }
+
+        private static void WriteMessageToConsole(string message)
+        {
+            Console.WriteLine();
+            Console.WriteLine(message);
         }
         
 
